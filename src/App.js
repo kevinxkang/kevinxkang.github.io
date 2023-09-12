3b4281dfed7af07a9ddc3fbde0bb66df85ad074e
kevinxkang <kevinkang1k1k@gmail.com>
2023-09-11 22:26:06 -0700

    code: fixing deployment issue (reinstall npm depn)

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
                 {/*default route*/}
                 <Route path="/" element={<Home />} />
                 <Route path="/home" element={<Home />}/>
-                <Route path="/about" element={<About />} />
+                {/*<Route path="/about" element={<About />} />*/}
                 <Route path="/work" element={<Portfolio />} />
                 <Route path="/resume" a={resume}/>
 
