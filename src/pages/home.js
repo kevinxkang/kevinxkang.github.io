9056c3763aa1cd8687bcd507d4851110097db7eb
kevinxkang <kevinkang1k1k@gmail.com>
2025-06-25 23:25:49 -0700

    misc: updating home-short-para with new information about self

--- src/pages/home.js
+++ src/pages/home.js
@@ -28,9 +28,8 @@ export default function Home() {
             <div className="home-text-container">
                 <div className="home-text-wrap">
                     <Fade bottom>
-                        <h2 className="home-h2-text"> Hey! It's Kevin 👋 </h2>
-                        <p className="home-short-para"> I'm a new grad from Brown University studying Computer Science, joining AWS
-                            as a Software Development Engineer!
+                        <h2 className="home-h2-text"> Hey! I'm Kevin 👋 </h2>
+                        <p className="home-short-para"> I'm currently a Software Engineer at Amazon AGI building Nova and shaping the future of human-computer interaction
                             {/*I enjoy living on the edge, designing and building software that*/}
                             {/*makes a positive impact on the world.*/}
                         </p>
@@ -39,7 +38,7 @@ export default function Home() {
                             a collection of my projects, experiments, and ramblings.
                         </p>
                         <p className="home-short-para">
-                            Enjoy your stay, but don't stay too long — I'll be wandering around!
+                            Enjoy your stay!
                         </p>
                     </Fade>
                 </div>
